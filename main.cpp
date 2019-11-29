ec68932a789190702a2dfbe1dd3aa1d6b523769c
arailly <yusuke.arailly@gmail.com>
2019-11-29 12:04:19 +0900

    :bug: config

--- main.cpp
+++ main.cpp
@@ -7,14 +7,14 @@ using namespace arailib;
 
 int main() {
     const auto config = read_config();
-    const string data_dir = config["data_dir"];
+    const string data_path = config["data_path"];
     const string knng_path = config["knng_path"];
-    const string save_path = config["save_dir"];
+    const string save_path = config["save_path"];
     const unsigned n = config["n"];
     const unsigned m = config["m"];
     const unsigned k = config["k"];
     const unsigned n_sample = config["n_sample"];
 
-    const auto nsg = create_nsg(data_dir, knng_path, m, k, n_sample, n);
+    const auto nsg = create_nsg(data_path, knng_path, m, k, n_sample, n);
     write_graph(save_path, nsg);
 }
