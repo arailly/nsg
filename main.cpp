b0d2fde7073f43712b23aee3b76b5325b53ff093
Yusuke Arai <yusuke.arailly@gmail.com>
2020-09-25 13:58:42 +0900

    :bug: fix

--- main.cpp
+++ main.cpp
@@ -6,18 +6,39 @@ using namespace std;
 using namespace arailib;
 
 int main() {
-    const auto config = read_config();
-    const string data_path = config["data_path"];
-    const string knng_path = config["knng_path"];
-    const string save_path = config["save_path"];
-    const unsigned n = config["n"];
-    const unsigned m = config["m"];
-    const unsigned l = config["l"];
-    const unsigned c = config["c"];
-    const unsigned n_sample = config["n_sample"];
-    const string df = config["distance"];
+    const int n = 1000, n_query = 10000;
+    const string base_dir = "/home/arai/workspace/";
+    const string data_path = base_dir + "dataset/deep/data10m/";
+    const string query_path = base_dir + "dataset/deep/query10k.csv";
+    const string graph_path = base_dir + "index/aknng/deep/data1m-d25/";
+    const string ground_truth_path = base_dir + "result/knn-search/scan/deep/data1m/k100/result.csv";
 
-    auto nsg = NSG(df);
-    nsg.build(data_path, knng_path, m, n_sample, n, l, c);
-    nsg.save(save_path);
+    const auto queries = load_data(query_path, n_query);
+    const auto ground_truth = load_neighbors(ground_truth_path, n_query, true);
+
+    int m = 40;
+
+    int k = 10;
+    int l = k;
+
+    auto index = NSG(m);
+    index.build(data_path, graph_path, n);
+
+    cout << "complete: build index" << endl;
+
+    SearchResults results;
+    for (const auto& query : queries) {
+        auto result = index.knn_search(query, k, l);
+        result.recall = calc_recall(result.result, ground_truth[query.id], k);
+        results.push_back(move(result));
+    }
+
+    const string save_postfix = "k" + to_string(k) +
+                                "-m" + to_string(m) +
+                                "-l" + to_string(l) + ".csv";
+    const string save_dir = base_dir + "result/knn-search/nsg/deep/data1m/k-vary/";
+    const string log_path = save_dir + "log-" + save_postfix;
+    const string result_path = save_dir + "result-" + save_postfix;
+    results.save(log_path, result_path);
+    }
 }
